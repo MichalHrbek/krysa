0e61a396346e81d0edebb2d77eabd7ee2b28ae92
Michal Hrbek <michal@dee.cz>
2025-03-03 18:54:10 +0100

    Only undercase ids

--- server/c2/uid.py
+++ server/c2/uid.py
@@ -2,7 +2,7 @@ from typing import Annotated
 from fastapi import Path
 from uuid import uuid4
 
-Uid = Annotated[str, Path(min_length=32,max_length=32,pattern=r"^[0-9a-fA-F]{32}$")]
+Uid = Annotated[str, Path(min_length=32,max_length=32,pattern=r"^[0-9a-f]{32}$")]
 
 def gen_uid() -> Uid:
 	return uuid4().hex
